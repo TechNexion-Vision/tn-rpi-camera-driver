de4e64f9dfa2f1dbb8b268aa58c5d3ed7a9133a9
nian.yang <nian.yang@technexion.com>
2024-04-02 16:00:52 +0800

    media: tevs: modified standby funciton check states.

--- drivers/media/i2c/tevs/tevs_main.c
+++ drivers/media/i2c/tevs/tevs_main.c
@@ -516,7 +516,7 @@ static int tevs_standby(struct tevs *tevs, int enable)
 		while (timeout < 100) {
 			tevs_i2c_read_16b(
 				tevs, HOST_COMMAND_ISP_CTRL_SYSTEM_START, &v);
-			if ((v & 0x100) == 0)
+			if ((v & 0xFF00) == 0x0000)
 				break;
 			if (++timeout >= 100) {
 				dev_err(&client->dev, "timeout: line[%d]v=%x\n",
@@ -532,7 +532,7 @@ static int tevs_standby(struct tevs *tevs, int enable)
 		while (timeout < 100) {
 			tevs_i2c_read_16b(
 				tevs, HOST_COMMAND_ISP_CTRL_SYSTEM_START, &v);
-			if ((v & 0x100) == 0x100)
+			if ((v & 0xFF00) == 0x0100)
 				break;
 			if (++timeout >= 100) {
 				dev_err(&client->dev, "timeout: line[%d]v=%x\n",
